6299180765198eef331a1b33e3b54f817addcdc4
unknown <parth.m@ahduni.edu.in>
2021-03-16 12:51:36 +0530

    small change

--- Flexpack/settings.py
+++ Flexpack/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
-import django.middleware.csrf.CsrfViewMiddleware
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
